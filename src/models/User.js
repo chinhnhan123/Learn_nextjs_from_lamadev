ad7e3c8ed3196ac939b2a0755b706672adc13d4c
chinhnhan123 <nhanchinh113@gmail.com>
2023-06-16 20:50:27 +0700

    Learn_nextjs_from_lamadev

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+var Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
